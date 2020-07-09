3ba0537c38695df4955524e461640ed83c0c7d54
Rajesh <stephen291192@gmail.com>
2020-07-09 18:25:38 +0530

    Demo node file

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("...Welcome my node.js page...");
+
+const notes = require('./sam.js');
+//  console.log("the sam file",notes);
+
+var res = notes.add(10,2);
+console.log("the 2 numbre add..",res);
+
+
+
---
+++ sam.js
@@ -0,0 +1,6 @@
+console.log("i am sample page..");
+
+module.exports.add = (a,b) => {
+    return a - b;  
+}
+
